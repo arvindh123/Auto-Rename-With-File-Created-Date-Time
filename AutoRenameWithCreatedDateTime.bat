b436a243d21c52b5038f4ea27a8ef78590ee25ee
Arvindh <arvindh123@users.noreply.github.com>
2019-05-28 11:00:41 +0530

    ""

--- AutoRenameWithCreatedDateTime.bat
+++ AutoRenameWithCreatedDateTime.bat
@@ -1,23 +1,26 @@
-@echo off 
+@ECHO OFF 
 REM Change the *.jpeg to what ever file you need.
 REM this will not rename files in sub folder
 REM to rename the files in sub folder Add "/r"  after "FOR" in next line , without double quotes
-FOR %%i IN (*.jpeg) DO (
+
+FOR %%i IN (*.jpg) DO (
 	SETLOCAL ENABLEDELAYEDEXPANSION
 	REM ECHO Renaming... File Name-%%i  DateTime-%%~ti Size-%%~zi FullPath%%~fi OnlyName-%%~ni Extension-%%~xi   
 	
 	SET FileName=%%i
+	SET OrgiFileName="!FileName!"
+	REM ECHO OrgiFileName: !OrgiFileName!
 	SET FileDt=%%~ti
 	SET FileDt=!FileDt:^:=-!
 	SET FileDt=!FileDt: =_!
 	SET FileDt=!FileDt:/=-!
-	REM echo FileDt : !FileDt!
-	SET NewFileName=%%~ni_!FileDt!%%~xi
+	REM ECHO FileDt : !FileDt!
+	SET NewFileName="%%~ni_!FileDt!%%~xi"
 	REM ECHO NewFileName : !NewFileName!
-	SET CompareFileName=!FileName:%FileDt%=!
+	SET CompareFileName=!FileName!!FileDt!
 	REM ECHO CompareFileName : !CompareFileName!
-	if !CompareFileName!==!FileName! (
-		echo Renaming... File Name-%%i to New File Name-!NewFileName!
-		RENAME  !FileName! !NewFileName!
+	IF NOT !CompareFileName!==!FileName! (
+		ECHO Renaming... File Name-%%i to New File Name-!NewFileName!
+		REN  !OrgiFileName!     !NewFileName!
 	)
 )
